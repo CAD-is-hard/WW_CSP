e78ac86ba7513d63eda40a1ae5ab7a64a44e6b83
CAD-is-hard <william.wilcox@ucas-edu.net>
2026-09-21 13:50:50 -0600

    Madlib

---
+++ silly_scentence.py
@@ -0,0 +1,33 @@
+# WW, Silly Sentences
+
+print("You are going to fill in a madlib, so add in the best words you can think of.")
+
+noun1 = input("Give me a noun: ").strip()
+
+infinitive_verb = input("Give me an infinitive verb (a verb with to in front of it, to run, to eat): ").strip()
+
+pronoun = input("Give me a pronoun: ").strip()
+
+noun2 = input("Give me a noun: ").strip()
+
+verb1 = input("Give me a verb: ").strip()
+
+noun3 = input("Give me a noun: ").strip()
+
+adjective1 = input("Give me an adjective: ").strip()
+
+noun4 = input("Give me a noun: ").strip()
+
+verb2 = input("Give me a verb: ").strip()
+
+adjective2 = input("Give me an adjective: ").strip()
+
+noun5 = input("Give me a noun: ").strip()
+
+noun6 = input("Give me a noun: ").strip()
+
+adjective3 = input("Give me an adjective: ").strip()
+
+noun7 = input("Give me a noun: ").strip()
+
+print("Once, there was a "+noun1+" whose dream in life was "+infinitive_verb+". Everyone told "+pronoun+" that "+noun2+" can't "+verb1+", but "+noun3+" was "+adjective1+". First, he went to "+noun4+". There, he "+verb2+", but that just made everyone "+adjective2+". From there, he made the long trip to "+noun5+", where to his surprise he discovered "+noun6+", Feeling "+adjective3+", he went back home, bringing with him a "+noun7+".")
