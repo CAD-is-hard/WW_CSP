a1f5c060c678e54b371a12eaed85acb8d1d0ad23
CAD-is-hard <william.wilcox@ucas-edu.net>
2026-09-24 13:51:31 -0600

    Loops

---
+++ loops.py
@@ -0,0 +1,23 @@
+# WW, Loops Notes
+import random
+# code that will repeat over and over again
+count = 1
+
+while count <= 10:
+    print(count)
+    count += 1
+
+
+goose = random.randint(1,11)
+ducks = 1
+
+while True:
+    print("duck")
+    if ducks == goose:
+        break
+    ducks += 1
+print("GOOSE!!!")
+
+
+siblings = ["Ela", "Eliza", "Thomas", "James", "Richard"]
+print(siblings)
